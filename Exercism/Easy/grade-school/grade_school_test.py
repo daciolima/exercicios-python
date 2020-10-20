56c89ee61744f38b9c7499ddb4231720034f963e
Dácio Lima <daciolima@MacBook-Pro.local>
2020-10-20 02:36:20 -0300

    grade_school

---
+++ Exercism/Easy/grade-school/grade_school_test.py
@@ -0,0 +1,69 @@
+import unittest
+
+from grade_school import School
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.0.1
+
+
+class GradeSchoolTest(unittest.TestCase):
+    # def test_adding_a_student_adds_them_to_the_sorted_roster(self):
+    def test_adicionar_um_aluno_na_lista_classificada(self):
+        school = School()
+        school.add_student(name="Aimee", grade=2)
+        expected = ["Aimee"]
+        self.assertEqual(school.roster(), expected)
+
+    # def test_adding_more_student_adds_them_to_the_sorted_roster(self):
+    def test_adicionar_mais_alunos_adicionar_na_lista_classificada_retornando_ordem_alfabetica(self):
+        school = School()
+        school.add_student(name="Blair", grade=2)
+        school.add_student(name="James", grade=2)
+        school.add_student(name="Paul", grade=2)
+        expected = ["Blair", "James", "Paul"]
+        self.assertEqual(school.roster(), expected)
+
+    # def test_adding_students_to_different_grades_adds_them_to_the_same_sorted_roster(
+    def test_adicionar_alunos_na_series_diferentes_e_adicionar_na_mesma_lista_classificada(self):
+        school = School()
+        school.add_student(name="Chelsea", grade=3)
+        school.add_student(name="Logan", grade=7)
+        expected = ["Chelsea", "Logan"]
+        self.assertEqual(school.roster(), expected)
+
+    # def test_roster_returns_an_empty_list_if_there_are_no_students_enrolled(self):
+    def test_retorna_uma_lista_vazia_se_nao_houver_alunos_matriculados(self):
+        school = School()
+        expected = []
+        self.assertEqual(school.roster(), expected)
+
+    # def test_student_names_with_grades_are_displayed_in_the_same_sorted_roster(self):
+    def test_nomes_de_alunos_com_notas_sao_exibidos_na_mesma_lista_classificada(self):
+        school = School()
+        school.add_student(name="Peter", grade=2)
+        school.add_student(name="Anna", grade=1)
+        school.add_student(name="Barb", grade=1)
+        school.add_student(name="Zoe", grade=2)
+        school.add_student(name="Alex", grade=2)
+        school.add_student(name="Jim", grade=3)
+        school.add_student(name="Charlie", grade=1)
+        expected = ["Anna", "Barb", "Charlie", "Alex", "Peter", "Zoe", "Jim"]
+        self.assertEqual(school.roster(), expected)
+
+    # def test_grade_returns_the_students_in_that_grade_in_alphabetical_order(self):
+    def test_nota_retorna_os_alunos_daquela_serie_em_ordem_alfabetica(self):
+        school = School()
+        school.add_student(name="Franklin", grade=5)
+        school.add_student(name="Bradley", grade=5)
+        school.add_student(name="Jeff", grade=1)
+        expected = ["Bradley", "Franklin"]
+        self.assertEqual(school.grade(5), expected)
+
+    # def test_grade_returns_an_empty_list_if_there_are_no_students_in_that_grade(self):
+    def test_retorna_uma_lista_vazia_se_nao_houver_alunos_na_serie(self):
+        school = School()
+        expected = []
+        self.assertEqual(school.grade(1), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
