47b01e736b6325a2000f8fd5d0f97aeb4df0997f
Dácio Lima <daciolima@MacBook-Pro.local>
2020-11-03 19:45:47 -0300

    Exercise: Acronym - Solved

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-## Python exercises including test.. (unittest or pytest).
+## Python exercises including test. (unittest or pytest).
 
-#### Category Level :
-##### Easy:
-    - Hello World; 
+#### Category Level:
+<p>
+    <img src="https://img.shields.io/badge/Easy Level%20-%231798c1.svg?&style=for-the-badge"/>
+    <img src="https://img.shields.io/badge/Medium Level%20-%23ffe866.svg?&style=for-the-badge"/>
+    <img src="https://img.shields.io/badge/Hard Level%20-%23FF6961.svg?&style=for-the-badge"/>
+
+</p>
+
+#### Exercises:
+<p>
+    <img src="https://img.shields.io/badge/Hello World %20-%231798c1.svg?&style=for-the-badge"/>
+    <img src="https://img.shields.io/badge/Acronym %20-%231798c1.svg?&style=for-the-badge"/>
+
+</p>    
+
+    
---
+++ acronym/acronym.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+import string
+import re
+
+
+# Usando regex
+def abbreviate(words):
+    lista = []
+    alfabeto = list(string.ascii_uppercase) + list(string.ascii_lowercase)
+    unir = ''
+    words_limpando = words.replace("'", '').replace('_', '')
+    recebendo_apenas_alfa_numerico = re.sub(r"\W+", ' ', words_limpando)
+    recebendo_fatiado = recebendo_apenas_alfa_numerico.split(' ')
+    for i in recebendo_fatiado:
+        lista.append(i[0])
+    retorna = unir.join(lista).upper()
+    return retorna
+
+
+# print(abbreviate("Casa's -  Blanca-Club unir _Not_"))
+
+
+# import string
+#
+#
+# # Sem regex
+# def abbreviate(words):
+#     lista = []
+#     unir = ''
+#     recebe_limpando = words.replace('_', '').replace(' - ', ' ').replace('-', ' ')
+#     recebe_fatiado = recebe_limpando.split(' ')
+#     letras = list(string.ascii_uppercase) + list(string.ascii_lowercase)
+#     for i in recebe_fatiado:
+#         if i[0] in letras:
+#             lista.append(i[0])
+#     retorna = unir.join(lista).upper()
+#     return retorna
+
+
+# print(abbreviate("Casa's - Blanca-Club unir _Not_"))
